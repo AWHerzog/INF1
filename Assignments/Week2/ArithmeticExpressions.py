395142342bf40eb06ee698ff8670d671ee812add
AWherzog <Herzogwalterabraham@gmail.com>
2024-09-24 14:29:29 +0200

    assignment2

---
+++ Assignments/Week2/ArithmeticExpressions.py
@@ -0,0 +1,6 @@
+def calculate(a, b, c, d):
+    
+    return a - (b**2 / (c -d * (a+b)))
+
+
+print(calculate(1, 2, 3, 4))
---
+++ Assignments/Week2/Greetings.py
@@ -0,0 +1,6 @@
+def generate_greeting(name, age):
+    
+    return f'Hello {name}, you are {age} years old!'
+
+
+print(generate_greeting("Hans", 37))
