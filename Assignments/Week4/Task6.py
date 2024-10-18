dc1514e4450fdfe2031f275e6f77e744a25e203d
AWherzog <Herzogwalterabraham@gmail.com>
2024-10-18 15:52:46 +0200

    week4 done

--- Assignments/Week4/Task6.py
+++ Assignments/Week4/Task6.py
@@ -1,19 +1,59 @@
 def is_valid_IPv4_octet(octet):
-    """Returns True if octet represents a valid IPv4 octet, False otherwise"""
     
+    if not octet.isdigit():
+        return False
+    
+    num = int(octet)
+
+    if num < 0 or num > 255:
+        return False
+
+    if octet[0] == "0" and len(octet) > 1:
+        return False
+    
+    return True
 
 def is_valid_IPv4(ip):
-    """Returns True if ip represents a valid IPv4 address, False otherwise"""
-    pass
+    
+    octets = ip.split(".")
+
+    if len(octets) != 4:
+        return False
+
+    for octet in octets:
+        if not is_valid_IPv4_octet(octet):
+            return False
+    
+    return True
 
 def is_valid_IPv6_hextet(hextet):
-    """Returns True if hextet represents a valid IPv6 hextet, False otherwise"""
-    pass
+    if len(hextet) < 1 or len(hextet) > 4:
+        return False
+    
+    try:
+        value = int(hextet, 16)
+        if value < 0 or value > 0xFFFF:
+            return False
+    except ValueError:
+        return False
+    
+    return True
 
 def is_valid_IPv6(ip):
-    """Returns True if ip represents a valid IPv6 address, False otherwise"""
-    pass
+    hextets = ip.split(":")
+
+    if len(hextets) != 8:
+        return False
+
+    for hextet in hextets:
+        if not is_valid_IPv6_hextet(hextet):
+            return False
+    
+    return True
 
 def is_valid_IP(ip):
-    """Returns True if ip represents a valid IPv4 or IPv6 address False otherwise"""
-    pass
+    
+    return is_valid_IPv4(ip) or is_valid_IPv6(ip)
+
+
+
---
+++ Assignments/Week5/Task1.py
@@ -0,0 +1,30 @@
+# The signatures of this class and its public methods are required for the automated grading to work.
+# You must not change the names or the List of parameters.
+# You may introduce private/protected utility methods though.
+class Calendar:
+
+    def __init__(self, event_id):
+        self.event_id = event_id
+        
+
+    def add_event(self, date_str, start_time, end_time, description):
+        pass
+
+    def get_events(self, date_str):
+        pass
+
+    def delete_event(self, event_id):
+        pass
+
+
+# You can play around with your implementation in the following body.
+# The contained statements will be ignored while evaluating your solution.
+if __name__ == "__main__":
+    cal = Calendar()
+    event_id_dinner = cal.add_event("2024-12-24", "18:00", "20:00", "Christmas Dinner with Family")
+    event_id_sleep = cal.add_event("2024-12-24", "21:00", "23:59", "Sleep")
+    print(cal.get_events("2024-12-24"))
+    cal.delete_event(event_id_dinner)
+    print(cal.get_events("2024-12-24"))
+    cal.delete_event(event_id_sleep)
+    print(cal.get_events("2024-12-24"))
