c6f743eb1457f0bcf19f52e0d140164dd44e5ae1
AWherzog <Herzogwalterabraham@gmail.com>
2024-10-22 13:46:58 +0200

    leet

--- Leetcode/PlusOne.py
+++ Leetcode/PlusOne.py
@@ -1,5 +1,31 @@
+
+
 def plusOne(digits):
 
-    pass
+    for i in range(len(digits)-1, -1, -1):
+    
+        if digits[i] < 9:
+             digits[i] += 1
 
-#do this next
+             return digits
+        
+        if digits[i] == 9: 
+            digits[i] = 0
+            return digits
+    
+    
+
+print(plusOne([1, 0, 9]))
+             
+        
+       
+        
+
+
+
+
+
+
+        
+
+
