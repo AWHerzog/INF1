4f61f2a100e6518c44e7cdb137301e336699f285
AWherzog <Herzogwalterabraham@gmail.com>
2024-10-29 12:52:36 +0100

    midterm

---
+++ Leetcode/Sorting/FindTheDifference.py
@@ -0,0 +1,24 @@
+
+def findTheDifference(s, t):
+    
+    if s == []:
+        return t
+    
+    
+    for i in list(enumerate(s)):
+        return i
+
+    return i
+            
+
+    
+
+  
+        
+
+
+
+
+
+
+print(findTheDifference(("abcd"), ("abcde")))
