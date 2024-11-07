83370b3ec402e55f8cb4cb5f9b6104420563ba45
AWherzog <Herzogwalterabraham@gmail.com>
2024-11-07 16:33:46 +0100

    pow3

--- Leetcode/Math/PowerOfThree.py
+++ Leetcode/Math/PowerOfThree.py
@@ -3,9 +3,8 @@ import math
 
 def isPowerOfThree(n):
     
+    if n <= 0:
+        return False
     while n % 3 == 0:
-        n /= 3
-
+        n //= 3
     return n == 1
-
-print(isPowerOfThree(27))
