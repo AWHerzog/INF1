7763e5daa7d473e7327599ed817ebc285afbe36a
AWherzog <Herzogwalterabraham@gmail.com>
2024-11-08 15:30:23 +0100

    mergesortedarray

---
+++ Leetcode/Sorting/MergeSortedArray.py
@@ -0,0 +1,19 @@
+
+def merge(nums1, m, nums2, n):
+    
+    nums1_trimmed = nums1[:m]
+
+    nums1.clear()
+
+    nums1.extend(nums1_trimmed)
+    nums1.extend(nums2)
+
+    nums1.sort()
+
+    return nums1    
+
+
+
+print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))
+print(merge([1], 1, [], 0))
+print(merge([0], 0, [1], 1))
