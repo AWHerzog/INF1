69a1e286a110d7b339a5730c403fda9bdefc5dc8
AWherzog <Herzogwalterabraham@gmail.com>
2024-11-20 15:51:09 +0100

    task6

--- Assignments/Week9/Task5.py
+++ Assignments/Week9/Task5.py
@@ -1,7 +1,10 @@
 
 def req_steps(num_disks):
-    pass
 
+    if num_disks == 1:
+        return 1
+    
+    return 2 * req_steps(num_disks -1) + 1
 
 
 print("For moving {} disks, {} steps are required.".format(3, req_steps(3)))
