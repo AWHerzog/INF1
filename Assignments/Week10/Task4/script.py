f98b242a641189209c98fb0880b595259c23038d
AWherzog <Herzogwalterabraham@gmail.com>
2024-11-28 15:47:14 +0100

    car

--- Assignments/Week10/Task4/script.py
+++ Assignments/Week10/Task4/script.py
@@ -5,7 +5,7 @@ from electric_car import ElectricCar
 from hybrid_car import HybridCar
 
 
-'''
+
 c = CombustionCar(40.0, 8.0)
 c.get_remaining_range() # 500
 c.drive(25.0)
@@ -16,7 +16,7 @@ except Warning:
     print("fuel is depleted")
 else:
     raise Warning("Here should have been raised a warning!")
-'''
+
 
 
 e = ElectricCar(25.0, 500.0)
@@ -25,10 +25,9 @@ e.charge(2.0)
 e.get_battery_status() # (22.0, 25)
 
 
-'''
+
 h = HybridCar(40.0, 8.0, 25.0, 500.0)
 h.switch_to_combustion()
 h.drive(600.0) # depletes fuel, auto-switch
 h.get_gas_tank_status() # (0.0, 40.0)
 h.get_battery_status() # (20.0, 25.0)
-'''
