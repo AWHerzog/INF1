9ab2f3dda94e2bd3cf1bb018fde61e61bdfb0926
AWherzog <Herzogwalterabraham@gmail.com>
2024-11-30 14:47:35 +0100

    Data structs

---
+++ Exam/HS23/Data Structures/Task2.py
@@ -0,0 +1,26 @@
+
+def padded_zip(lists, padding=None):
+    if not lists:
+        return ()
+    res = []
+
+    for i in range(max(len(l) for l in lists)):
+        this = []
+        for l in lists:
+            try:
+                this.append(l[i])
+            except IndexError:
+                this.append(padding)
+        res.append(tuple(this))
+    return res 
+
+        
+    
+    
+
+
+l1 = [1, 2, 3, 4, 5]
+l2 = [1.5, 2.5, 3.5, 4.5]
+l3 = ["please", "send", "help"]
+print(padded_zip([l1, l2, l3]))
+print(padded_zip([l1, l2, l3, l1], "!"))
