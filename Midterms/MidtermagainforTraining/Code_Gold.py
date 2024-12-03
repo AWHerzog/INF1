7c162c21b7db8bb4898799c083ba52ed8c1a06fd
AWherzog <Herzogwalterabraham@gmail.com>
2024-12-03 16:14:17 +0100

    Midterm

---
+++ Midterms/MidtermagainforTraining/Code_Gold.py
@@ -0,0 +1,8 @@
+def caser(s):
+    return s.upper() if len(s) > 5 else s.lower()
+
+
+print( caser("Hello") )
+print( caser("Bananas") )
+print( caser("AbC") )
+print( caser("i don't know!") )
---
+++ Midterms/MidtermagainforTraining/Comprehension.py
@@ -0,0 +1,7 @@
+
+def divisor_finder(numbers, potential_divisors):
+    return {num: [d for d in potential_divisors if num % d == 0] for num in numbers }
+
+print(divisor_finder([12], [2, 3, 4, 5]))
+print(divisor_finder([12, 14, 16], [2, 3, 4]))
+print(divisor_finder([30, 42, 70], [2, 3, 5, 7]))
