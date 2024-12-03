4e9398db402a79b8dcc9b089cb97e7313558a051
AWherzog <Herzogwalterabraham@gmail.com>
2024-12-03 17:16:00 +0100

    s

---
+++ Exam/HS23/Data Structures/Task1.py
@@ -0,0 +1,22 @@
+
+def merge_dicts(dicts, reverse_priority=False):
+    
+    res = {}
+    if  reverse_priority:
+        dicts = reversed(dicts)
+    
+    for d in dicts:
+        for k, v in d.items():
+            res[k] = v
+    return res
+        
+
+
+
+
+
+d1 = {1: "a", 2: "b", 3: "c"}
+d2 = {1: 1, 20: 2, 300: 3}
+d3 = {1: "please", 2: "send", 300: "help"}
+print(merge_dicts([d1, d2, d3]))
+print(merge_dicts([d1, d2, d3], True))
---
+++ Exam/HS23/Data Structures/Task2.py
@@ -0,0 +1,11 @@
+
+def padded_zip(lists, padding=None):
+    
+    pass
+
+
+l1 = [1, 2, 3, 4, 5]
+l2 = [1.5, 2.5, 3.5, 4.5]
+l3 = ["please", "send", "help"]
+print(padded_zip([l1, l2, l3]))
+print(padded_zip([l1, l2, l3, l1], "!"))
